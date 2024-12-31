e9b212a7db5fb967d08dc14ec6a2fd8b1ea043c8
Hailey <hailey@not-an-email-address.fake>
2024-12-31 16:16:42 -0600

    stuff

---
+++ demo_task_chain.c
@@ -0,0 +1,108 @@
+#include "task.h"
+#include <stdio.h>
+
+
+/*
+
+
+
+            THIS IS A WORK IN PROGRESS
+
+
+
+*/
+
+
+struct TState
+{
+    int times;
+};
+
+void T1(struct TaskRunner *run, struct TaskInfo *self, void *data);
+void T2(struct TaskRunner *run, struct TaskInfo *self, void *data);
+void T3(struct TaskRunner *run, struct TaskInfo *self, void *data);
+
+void T3(struct TaskRunner *run, struct TaskInfo *self, void *data)
+{
+    struct TState *s = data;
+    puts("T3");
+
+    if (s->times)
+    {
+        /* do some task work */
+        s->times -= 1;
+    }
+    else
+    {
+        TaskCompleate(run, self, 0);
+        self->method = T1;
+        struct TState *ts = self->task_state;
+        ts->times = 5;
+        TaskRunner_AddTask(run, self);
+    }
+}
+void T2(struct TaskRunner *run, struct TaskInfo *self, void *data)
+{
+    struct TState *s = data;
+    puts("T2");
+
+    if (s->times)
+    {
+        /* do some task work */
+        s->times -= 1;
+    }
+    else
+    {
+        TaskCompleate(run, self, 0);
+        self->method = T3;
+        struct TState *ts = self->task_state;
+        ts->times = 5;
+        TaskRunner_AddTask(run, self);
+    }
+}
+
+void T1(struct TaskRunner *run, struct TaskInfo *self, void *data)
+{
+    struct TState *s = data;
+
+    puts("T1");
+
+    if (s->times)
+    {
+        /* do some task work */
+        s->times -= 1;
+    }
+    else
+    {
+        TaskCompleate(run, self, 0);
+        self->method = T2;
+        struct TState *ts = self->task_state;
+        ts->times = 5;
+        TaskRunner_AddTask(run, self);
+    }
+}
+
+int main()
+{
+    struct TaskRunner r = {0};
+    struct TaskInfo s = {.method = T1, .task_state = &(struct TState){.times = 5}};
+
+    TaskRunner_AddTask(&r, &s);
+
+    while (TaskRunner_HasTasks(&r))
+    {
+        TaskRunner_RunTasks(&r);
+    }
+}
+
+// --------------- WHAT IM KINDA THINKING ---------------
+
+void Schedule(struct TaskInfo *this, struct TaskInfo *after)
+{
+    
+}
+
+int _main()
+{
+    Schedule(T1, NULL);
+}
